82327557cbed3b7173c787f6c49029898ab25529
edgarburgues <62017333+edgarburgues@users.noreply.github.com>
2023-05-04 15:40:16 +0200

    commit

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="h-screen min-h-screen flex flex-col dark:bg-slate-900 dark:text-white adminLayout">
+        <nav class="p-4 flex justify-between items-center ">
+            <img class="w-52" src="~/assets/logo.webp" alt="Nuxt.js" />
+
+
+        </nav>
+        <hr>
+        <main class="h-full">
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+
+const accountPage = () => {
+    navigateTo('/account');
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,48 @@
+
 <template>
-    <h1>Home Page</h1>
+    <title>Index | Capitol Formación Profesional</title>
+    <div class="grid grid-cols-5 h-full">
+        <div class="col-span-4 flex justify-center items-center">
+            <h1 class="text-5xl font-bold">Simple Season</h1>
+        </div>
+        <div class="bg-slate-200 dark:bg-slate-600">
+            <div class="flex flex-col justify-center items-center h-full">
+                <h1 class="text-2xl font-bold">Login</h1>
+                <div class=" w-5/6">
+                    <div class="mt-2">
+                        <input v-model="credentials.email" class="w-full rounded p-2 text-slate-900" type="email"
+                            placeholder="email" />
+                    </div>
+                    <div class="mt-2">
+                        <input v-model="credentials.password" class="w-full rounded p-2 text-slate-900" type="password"
+                            placeholder="password" />
+                    </div>
+                    <div class="mt-2">
+                        <button @click="login"
+                            class="w-full rounded p-2 bg-slate-200 hover:bg-slate-300 border dark:border-none dark:bg-slate-700 dark:hover:bg-slate-500"
+                            type="submit">Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script setup>
 
+
+
+<script setup lang="ts">
+
+definePageMeta({
+    layout: 'default',
+})
+
+const credentials = reactive({
+    email: '',
+    password: ''
+})
 
 
 
 </script>
+ 
